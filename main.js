aa4491973e91283165b31888f03142d51aa6b73e
Dmitry Chizhov <chizhov.dee@gmail.com>
2014-04-01 00:02:09 +0600

    added spammer js file

--- main.js
+++ main.js
@@ -1,37 +1,94 @@
-global.$ = $;
-global._ = require('underscore');
-var gui = require('nw.gui');
+// подключаем модули
+var Spammer = require('spammer'); // основная логика программы
+var gui = require('nw.gui'); // node-webkit gui
+var View = require('view');
 
-global.logins = [
-  //['chizhov_dima', 'chizhov.dee@gmail.com', 'chizhov777'],
-  ['218027326', '79068914818', 'therapsida1'],
-  ['248635139', '79226341411', 'therapsida2']
+// определение глобальных переменных
+global._ = require('underscore');
+
+// Данные для авторизации в Вконтакте
+// login, password
+global.authorization_data = [
+  //[chizhov.dee@gmail.com', 'chizhov777'],
+  ['79068914818', 'therapsida1'],
+  ['79226341411', 'therapsida2']
 ];
 
-global.login_number = 0;
-
-global.currentUser = {};
-
-global.auth_user;
-
+// группы в Вконтакте из которых мы будем выбирать участников для спама
 global.groups = [
   'easeofdisgust'
 ];
 
-global.group_number = 0;
+// эта основная группа с которой мы будем синхронизировать участников с базой данных
+global.main_group = 'therapsida';
 
-global.currentGroup = {};
+global.started = false; // Работа спаммера не запущена по умолчанию
 
-global.success_added = 0;
-
-//var User = require('user');
-var Application = require('./application');
-
-// global.currentUser = new User();
+var view = new View();
+var spammer = new Spammer();
 
 $(function(){
   $('title').text(gui.App.manifest.name + ' v-' + gui.App.manifest.version);
 
-  new Application().show();
+  window.app_el       = $("#application"),
+  window.sidebar_el   = $('#sidebar');
+  window.container_el = $("#container"),
+
+  container.once('click', 'button.start:not(.disabled)', onStartClick);
 });
 
+function onStartClick(e){
+  $(e.currentTarget).addClass('disabled');
+
+  spammer.start();
+}
+
+
+// рендеры
+
+function renderTemplate(filename, args){
+  return view.render(filename, _.extend({}, this, args));
+}
+
+function renderContent(){
+  container_el.html(renderTemplate('content.ejs'));
+}
+
+function renderSidebar(){
+  sidebar_el.html(renderTemplate('sidebar.ejs'));
+}
+
+// _Class.prototype.renderModal = function(img, sid, callback){
+//   var self = this;
+
+//   this.modal.html(this.renderTemplate('modal.ejs', {img: img, sid: sid}));
+
+//   this.modal.show();
+
+//   this.modal.find('button.submit').one('click', function(){
+//     self.modal.hide();
+
+//     callback(self.modal.find('input[name=captcha_text]').val());
+//   });
+// }
+
+// Функции для вывода операций в окно программы
+
+function primaryLog(text){
+  var date = new Date();
+  var time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+  var log = this.container.find('.primary.log .text');
+
+  log.text('');
+
+  log.append(time + " - " + text + "</br>");
+
+  this.secondaryLog(text)
+}
+
+function secondaryLog(text){
+  var date = new Date();
+  var time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+  this.container.find('.secondary.log .text').append(time + " - " + text + "</br>");
+}
